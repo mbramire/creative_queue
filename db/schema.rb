5b0bab064868eb0d18a6966b3e4650c1d710767c
Matt Ramirez <mbramire@live.com>
2014-02-12 10:10:26 -0500

    fix to select style

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140212133733) do
+ActiveRecord::Schema.define(version: 20140211204140) do
 
   create_table "admin_users", force: true do |t|
     t.string   "first_name",                      null: false
@@ -961,7 +961,6 @@ ActiveRecord::Schema.define(version: 20140212133733) do
     t.integer  "quote"
     t.boolean  "ordered",            default: false
     t.boolean  "processed",          default: false
-    t.integer  "asi_number"
   end
 
   add_index "virtual_requests", ["artist_id"], name: "index_virtual_requests_on_artist_id", using: :btree
