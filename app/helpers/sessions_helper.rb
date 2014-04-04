fb18dbb10e1ab21893661a55dcc55b3cabb4c642
Matt Ramirez <mbramire@live.com>
2014-04-04 13:24:03 -0400

    fixed error in session_helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -33,14 +33,14 @@ module SessionsHelper
   end
 
   def admin_user
-    unless current_user.admin
+    unless current_user && current_user.admin
       flash[:error] = "Requires admin privledges."
       redirect_to root_path
     end
   end
 
   def journalbooks_user
-    unless current_user.journalbooks
+    unless current_user && current_user.journalbooks
       flash[:error] = "Requires Journalbooks privledges."
       redirect_to root_path
     end
